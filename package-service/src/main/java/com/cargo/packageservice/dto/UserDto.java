3adbe58136bc3ad5dd9dac79064e0b2d18747a6e
Alper Uğurcan <ugurcan.alper@outlook.com>
2024-04-07 23:05:05 +0300

    add UserDto to Package Service

---
+++ package-service/src/main/java/com/cargo/packageservice/dto/UserDto.java
@@ -0,0 +1,9 @@
+package com.cargo.packageservice.dto;
+
+public class UserDto {
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String email;
+    private Long identificationNumber;
+}
