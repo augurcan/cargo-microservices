de8d8c9a4cda17595710038b863cc5970bd413ca
Alper Uğurcan <ugurcan.alper@outlook.com>
2024-04-09 15:17:30 +0300

    enhancement: Add findByIdentificationNumber method to UserRepository

--- user-service/src/main/java/com/cargo/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/cargo/userservice/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.cargo.userservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByIdentificationNumber(long identificationNumber);
 }
